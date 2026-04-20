e598208790455d0466e1878cb2a65cbd0719b1bc
anna.bielskaya <anna.bielskaya@gmail.com>
2026-04-20 17:51:54 +0300

    Solved dat timer trick

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const handleStart = () => {
+    setTimerStarted(true);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      setTimerStarted(false);
+    }, targetTime * 1000);
+  };
+
+  const handleStop = () => {
+    setTimerStarted(false);
+    clearTimeout(timer.current);
+    setTimerExpired(false);
+  };
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && !timerStarted && <p>You Lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge / Stop Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop Challenge" : "Start Challenge"}
+        </button>
       </p>
-      <p className="active">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 };
